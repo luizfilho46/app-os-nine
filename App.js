12a8514fa11855e10602243ae3ad665505d333d3
luiz filho <luiz.filho@mutantbr.com>
2019-10-13 12:02:31 -0300

    Login view done

--- App.js
+++ App.js
@@ -1,35 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import Routes from './src/routes'
 
-const App = () => {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text>Luiz Filho 44 - Reactivando</Text>
-      </View>
-    </>
-  );
+export default function App () {
+  return <Routes />
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
-
-export default App;
---
+++ src/pages/List.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, Text, StyleSheet, Image, AsyncStorage } from 'react-native'
+import logo from '../assets/logo.png'
+export default function List () {
+    const [techs, setTechs] = useState([])
+
+    useEffect( () => {
+        AsyncStorage.getItem('techs').then(storageTechs => {
+            const techsArray = storageTechs.split(',').map( tech => tech.trim())
+            setTechs(techsArray)
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    }
+})
