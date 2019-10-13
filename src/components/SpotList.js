9c4d21104fe1f163f96ba1c998bfff568d78bbe3
luiz filho <luiz.filho@mutantbr.com>
2019-10-13 14:35:17 -0300

    added routes all

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import { withNavigation } from 'react-navigation'
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native'
 
 import api from '../services/api'
 
-export default function SpotList ({ tech }) {
+function SpotList ({ tech, navigation }) {
     const [spots, setSpots] = useState([])
 
     useEffect( () => {
@@ -16,8 +17,8 @@ export default function SpotList ({ tech }) {
         loadSpots()
     }, [])
 
-    function handleNavigate () {
-        
+    function handleNavigate (id) {
+        navigation.navigate('Book', { id })
     }
 
     return (
@@ -34,7 +35,7 @@ export default function SpotList ({ tech }) {
                     <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
                     <Text style={styles.company}>{ item.company }</Text>
                     <Text style={styles.price}>{ item.price ? `R$${item.price}/dia` : 'GRATUITO' }</Text>
-                    <TouchableOpacity onPress={ () => {} } style={styles.button}>
+                    <TouchableOpacity onPress={ () => handleNavigate( item._id )} style={styles.button}>
                         <Text style={styles.buttonText}>Solicitar Reserva</Text>
                     </TouchableOpacity>
                 </View>
@@ -103,3 +104,5 @@ const styles = StyleSheet.create({
         fontSize: 15,
     }
 })
+
+export default withNavigation(SpotList)
